d7c5c42855ea8a18796864792dd3422bb77a12ea
KhushiJain123 <101305934+KhushiJain123@users.noreply.github.com>
2022-11-10 14:17:08 +0800

    Add files via upload

---
+++ Overriding.java
@@ -0,0 +1,23 @@
+//16. write a java program to demonstrate use of method overriding
+class Parent {
+ void show()
+ {
+ System.out.println("Parent's show()");
+ }
+}
+class Child extends Parent {
+ @Override
+ void show()
+ {
+ System.out.println("Child's show()");
+ }
+}
+class Overriding{
+ public static void main(String[] args)
+ {
+ Parent obj1 = new Parent();
+ obj1.show();
+ Parent obj2 = new Child();
+ obj2.show();
+ }
+}
